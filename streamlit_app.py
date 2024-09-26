89e831f296bc8e6cc6b6fc569a3df03f37016e13
pzhang-sr <pzhang@spauldingridge.com>
2024-09-26 09:42:49 -0500

    Update streamlit_app.py
    Adding Fruityvice API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -14,11 +14,9 @@ name_on_order = st.text_input('Name on Smoothie: ')
 st.write('The name of your smoothie will be: ', name_on_order)
 
 
-# session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: '
@@ -28,22 +26,13 @@ ingredients_list = st.multiselect(
 
 ## remember is __ is the same as is not null in SQL lingo LOL 
 if ingredients_list: 
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
-
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
             ingredients_string += ' ' + fruit_chosen + ' '
 
-    # st.write(ingredients_string)
-
- 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                 values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
     
-    # st.write(my_insert_stmt)
-    # st.stop()
-
     time_to_insert = st.button("Submit Order")
 
     if time_to_insert:
@@ -51,9 +40,8 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 
-
-
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 
 
