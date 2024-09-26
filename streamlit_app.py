52a07948515b71ded55cb6e09c7788f6329c42a3
pzhang-sr <pzhang@spauldingridge.com>
2024-09-26 09:47:50 -0500

    Update streamlit_app.py
    Adding fruit_chosen API

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,10 @@ ingredients_list = st.multiselect(
 ## remember is __ is the same as is not null in SQL lingo LOL 
 if ingredients_list: 
     ingredients_string = ''
+    
     for fruit_chosen in ingredients_list:
         ingredients_string += ' ' + fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
