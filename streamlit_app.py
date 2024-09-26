9bd5906ce4e11a7031373f9e0c9c444b6ad69f2c
pzhang-sr <pzhang@spauldingridge.com>
2024-09-26 09:46:14 -0500

    Update streamlit_app.py
    Cleaning up code & spacing

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,12 @@ ingredients_list = st.multiselect(
 if ingredients_list: 
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
-            ingredients_string += ' ' + fruit_chosen + ' '
+        ingredients_string += ' ' + fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
+
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                 values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
@@ -38,13 +43,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
-
 
 
 
